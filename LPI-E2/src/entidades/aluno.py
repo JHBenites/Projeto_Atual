480fc80bd356c9de885f2e476cfcf32a23602e15
Jhonatan Benites <jhonatanarsbenites@gmail.com>
2023-04-16 20:21:37 -0400

    Finalização do cadastro_alunos.py

--- LPI-E2/src/entidades/aluno.py
+++ LPI-E2/src/entidades/aluno.py
@@ -11,7 +11,27 @@ class Aluno:
         self.estrangeiro = estrangeiro
 
     def __str__(self):
-        return self.nome + ' -- ano de nascimento: ' +str(self.ano_nascimento)
-    + ' - sexo: ' + self.__to_str_sexo++()\
-    +' - estado civil: ' + self.__to_str_estado_civil__()
+        return self.nome + ' -- ano de nascimento: ' + str(self.ano_nascimento)\
+    + ' - sexo: ' + self.__to_str_sexo__()\
+    +' - estado civil: ' + self.__to_str_estado_civil__()\
     +self.__to_str_estrangeiro__()
+
+    def __to_str_sexo__(self):
+        if self.sexo == 'M': return 'masculino'
+        elif self.sexo == 'F': return 'feminino'
+        else: return 'indefinido'
+
+    def __to_str_estado_civil__(self):
+        if self.sexo == 'M': return self.estado_civil
+        elif self.sexo == 'F':
+            if self.estado_civil != 'indefinido': return self.estado_civil[:-1] + 'a'
+            else: return 'indefinido'
+        else: return self.estado_civil + '/a'
+
+    def __to_str_estrangeiro_(self):
+        if not self.estrangeiro: return ''
+        else:
+            if self.sexo == 'M': return ' - estrangeiro'
+            elif self.sexo == 'F': return ' - estrangeira'
+            else: return ' - estrangeiro/a'
+   
