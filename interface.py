dbbe23c973cca77605215eb9850de5e31901c16e
Jhonatan Benites <jhonatanarsbenites@gmail.com>
2023-06-16 15:19:54 -0400

    não vai daaaaar

--- interface.py
+++ interface.py
@@ -3,10 +3,13 @@ from tkinter import *
 import random
 from tkinter import messagebox
 
+import interface
+
 root = tk.Tk()
 root.title('Aceitas?')
 root.geometry('600x600')
 root.configure(background='#ffc8dd')
+
 
 def move_button_1(e):
     if abs(e.x - button_1.winfo_x()) < 50 and abs(e.y - button_1.winfo_y()) < 40:
@@ -17,21 +20,21 @@ def move_button_1(e):
 def accepted():
     messagebox.showinfo('Aceite, você é', 'No amor você foi desafiada no hardcore!')
 
-
 def denied():
-    messagebox.showinfo('Aceite, você é', 'No amor você foi desafiada no hardcore!')
     button_1.destroy()
 
 margin = Canvas(root, width=500, bg='#ffc8dd', height=100,
                bd=0, highlightthickness=0, relief='ridge')
 margin.pack()
-text_id = Label(root, bg='#ffc8dd', text='Você é feia?',
+text_id = Label(root, bg='#ffc8dd', text='teste?',
                 fg='#590d22', font=('Montserrat', 24, 'bold'))
 text_id.pack()
 button_1 = tk.Button(root, text='Não', bg='#ffb3c1', command=denied,
                      relief=RIDGE, bd=3, font=('Montserrat', 8, 'bold'))
-button_1.pack()
-root.bind('<Motion>', move_button_1)
+if interface.button_1:
+    button_1.pack()
+    root.bind('<Motion>', move_button_1)
+
 button_2 = tk.Button(root, text='Sim', bg='#ffb3c1', relief=RIDGE,
                      bd=3, command=accepted, font=('Montserrat', 14, 'bold'))
 button_2.pack()
