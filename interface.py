e415fa5e4955e2b24d0db50482f1b636358e9370
JHBenites <jhonatanarsbenites@gmail.com>
2023-06-17 18:05:19 -0400

    fazendo

--- interface.py
+++ interface.py
@@ -8,6 +8,9 @@ root.title('Aceitas?')
 root.geometry('600x600')
 root.configure(background='#ffc8dd')
 
+global teste1
+teste = 1
+
 def move_button_1(e):
     if abs(e.x - button_1.winfo_x()) < 50 and abs(e.y - button_1.winfo_y()) < 40:
         x = random.randint(0, root.winfo_width() - button_1.winfo_width())
@@ -15,10 +18,12 @@ def move_button_1(e):
         button_1.place(x=x, y=y)
 
 def accepted():
-    messagebox.showinfo('Peitos que não balançam, não são peitos são peitos de verdade')
+    messagebox.showinfo('', 'tente o outro botão')
 
 def denied():
-    button_1.destroy()
+    button_1.pack_forget()
+    button_1.config(text='FOI')
+    teste1 = False
 
 margin = Canvas(root, width=500, bg='#ffc8dd', height=100,
                bd=0, highlightthickness=0, relief='ridge')
@@ -28,8 +33,11 @@ text_id = Label(root, bg='#ffc8dd', text='Você é noob?',
 text_id.pack()
 button_1 = tk.Button(root, text='Não', bg='#ffb3c1', command=denied,
                      relief=RIDGE, bd=3, font=('Montserrat', 12, 'bold'))
+
 button_1.pack()
-root.bind('<Motion>', move_button_1)
+if teste1 == True:
+    root.bind('<Motion>', move_button_1)
+
 button_2 = tk.Button(root, text='Sim', bg='#ffb3c1', relief=RIDGE,
                      bd=12, command=accepted, font=('Montserrat', 16, 'bold'))
 button_2.pack()
